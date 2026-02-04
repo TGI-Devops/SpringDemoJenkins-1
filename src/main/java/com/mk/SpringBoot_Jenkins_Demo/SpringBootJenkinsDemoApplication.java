4d3dce1e73d45630201effd1f0a0cba53b5870a7
TGI-Devops <talentgrowthinitiativetgi@gmail.com>
2026-02-04 11:12:58 +0530

    Update SpringBootJenkinsDemoApplication.java

--- src/main/java/com/mk/SpringBoot_Jenkins_Demo/SpringBootJenkinsDemoApplication.java
+++ src/main/java/com/mk/SpringBoot_Jenkins_Demo/SpringBootJenkinsDemoApplication.java
@@ -5,12 +5,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootJenkinsDemoApplication {
-
+private static final Logger logger =
+            LogManager.getLogger(SpringBootJenkinsDemoApplication .class);
 	public static void main(String[] args) {
+
+		 logger.info("Application started");
 		SpringApplication.run(SpringBootJenkinsDemoApplication.class, args);
-		String s=null;
-		s.length();
+
+		 logger.info("Application is working  fine");
+		
 		System.out.println("Welcome");
+
+		add();
 	}
+
+	
+	static void add(){
+
+ logger.info("add method started ");
+int a=10;
+int b=20;
+int c=a+b;
+ System.out.println(c)
+ logger.info("add method ended ");
+
+}
 
 }
